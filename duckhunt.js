705810654b3dd3d8171dea0b67366ba0bec28941
dadurand <ha.halo@live.fr>
2019-10-08 12:31:01 +0200

    ajouter 10 canards

--- duckhunt.js
+++ duckhunt.js
@@ -1,23 +1,46 @@
 
 var gameZone = document.createElement("section")
 
-var birdElement = document.createElement("div")
-birdElement.id =  "object"
+// var birdElement = document.createElement("div");
+// birdElement.className =  "object";
+for (let i = 0; i < 10; i++){
+    var birdElement = document.createElement("div");
+    birdElement.className =  "object";
+    gameZone.appendChild(birdElement);
+
+    birdElement.style.top = Math.random() * 696 + "px";
+    birdElement.style.left = Math.random() * 1550 + "px";
+}
 
 document.body.appendChild(gameZone);
-gameZone.appendChild(birdElement)
+// gameZone.appendChild(birdElement);
+
+birdElement.style.top = Math.random() * 450 + "px";
+birdElement.style.left = Math.random() * 550 + "px";
+
+var temp = 1;
 
 
-
-for (let i = 1 ; i<100; i++) {
-    var temp = 1
+setInterval(function(){
     
-    setInterval(function(){
-       birdElement.style.top = temp + "px";
-       birdElement.style.left = temp + "px";
-       temp++
-    },1000)
-    
+    birdElement.style.top = Math.random() * 696 + "px";
+    birdElement.style.left = Math.random() * 1550 + "px";
+    birdElement.style.transitionDuration = "1s";
+    temp++
+},1000)
 
+
+function killDuck(){
+    this.remove()
+
+    birdElement = document.createElement("div");
+    birdElement.className =  "object";
+    gameZone.appendChild(birdElement);
+    birdElement.style.top = Math.random() * 696 + "px";
+    birdElement.style.left = Math.random() * 1550 + "px";
+
+    birdElement.addEventListener("click", killDuck);
 }
+
+birdElement.addEventListener("click", killDuck);
 
